e194f123e72e84864913d9309f1352af0f45e50c
William Fredlund <wfre@kth.se>
2025-04-03 17:24:10 +0200

    first woking version

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from neural_network import NeuralNetwork
+from data import get_mnist
 
 epochs = 3
+correct = 0
 
 def main() -> None:
-    pass
+    inputs, labels = get_mnist()
+    
+    print('input shape: ', inputs.shape[1])
+    print('label shape: ', labels.shape[1])
+    
+    first_layer = (inputs.shape[1], 20)
+    output_layer = (20, labels.shape[1])
+    nn = NeuralNetwork([first_layer, output_layer])
+
+    for epoch in range(epochs):
+        for input, label in zip(inputs, labels):
+            input.shape += (1,)
+            label.shape += (1,)
+            
+            #forward propagation
+            classification = nn.classify(input)
+            error = nn.cost(label)
+            correct += int(classification == np.argmax(label))
+
+            #back propagation
+            nn.back_propagate(input, label)
+        print(f"Acc: {round((correct / inputs.shape[0]) * 100, 2)}%")
+        correct = 0
+
+
 
 if __name__ == "__main__":
     main()
--- neural_network.py
+++ neural_network.py
@@ -1,12 +1,12 @@
 from layer import Layer
 import numpy as np
-from typing import List
+from typing import List, Tuple
 from numpy.typing import NDArray
 
 class NeuralNetwork:
 
-    def __init__(self, layer_sizes: List[int]):
-        self.layers: List[Layer] = [Layer() for _ in layer_sizes]
+    def __init__(self, layer_sizes: List[Tuple[int, int]]):
+        self.layers: List[Layer] = [Layer(x, y) for x,y in layer_sizes]
         self.learn_rate = 0.01
 
     def calculate_outputs(self, inputs: NDArray) -> NDArray:
@@ -24,3 +24,21 @@ class NeuralNetwork:
     def cost(self, inputs: NDArray, label: NDArray) -> float:
         self.outputs = self.calculate_outputs(inputs)
         return 1 / len(self.outputs) * np.sum((self.outputs - label) ** 2, axis=0)
+    
+    def back_propagate(self, input: NDArray, label: NDArray) -> None:
+        delta = self.outputs - label
+        self.layers[-1].weights += -self.learn_rate * delta @ np.transpose(self.layers[-2].outputs)
+        self.layers[-1].biases += -self.learn_rate * delta
+        
+        for i in range(len(self.layers) - 2, 0, -1):
+            prev_layer = self.layers[i + 1]
+            curr_layer = self.layers[i]
+            next_layer = self.layers[i - 1]
+
+            delta = np.transpose(prev_layer.weights) @ delta * (curr_layer.outputs * (1 - curr_layer.outputs))
+            curr_layer.weights += -self.learn_rate * delta @ np.transpose(next_layer.outputs)
+            curr_layer.biases += -self.learn_rate * delta
+
+        delta = self.layers[0].weights.T @ delta * (self.layers[1].weights.outputs * (1 - self.layers[1].weights.outputs))
+        self.layers[0].weights += -self.learn_rate * delta @ input.T
+        self.layers[0].biases += -self.learn_rate * delta
